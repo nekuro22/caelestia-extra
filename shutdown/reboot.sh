3b9e0066fda1c056361ce1b5e9b648538e88f11b
nekuro22 <nikicrafter2022@gmail.com>
2026-01-03 17:19:41 +0100

    u

--- shutdown/reboot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Dieses Skript gibt "Press Enter to Reboot" aus und startet das System neu, wenn Enter gedrückt wird.
-
-echo "Press Enter to Reboot"
-read -r # Warten auf Benutzereingabe
-sudo reboot
--- shutdown/shutdown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Dieses Skript gibt "Press Enter to Shutdown" aus und fährt das System herunter, wenn Enter gedrückt wird.
-
-echo "Press Enter to Shutdown"
-read -r # Warten auf Benutzereingabe
-sudo shutdown now
